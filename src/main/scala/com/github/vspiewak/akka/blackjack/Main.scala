c9fd180580ad6fdf01d83e43cda9b71d14624113
mdulac <mat.dulac@gmail.com>
2018-01-18 11:58:38 +0100

    Refactoring :  - renaming  - Seq -> List  - case classes are now final  - cut card is now set randomly  - actor message is typed  - can take n cards

--- src/main/scala/com/github/vspiewak/akka/blackjack/Main.scala
+++ src/main/scala/com/github/vspiewak/akka/blackjack/Main.scala
@@ -1,33 +1,52 @@
 package com.github.vspiewak.akka.blackjack
 
 import akka.actor._
+import com.github.vspiewak.akka.blackjack.Deck.{new52, shuffle}
+import com.github.vspiewak.akka.blackjack.Shoe.Take
 
-import scala.runtime.BoxedUnit
 import scala.util.Random
 
 sealed abstract class Suite
+
 case object ♠ extends Suite
+
 case object ♥ extends Suite
+
 case object ♣ extends Suite
+
 case object ♦ extends Suite
 
 sealed abstract class Rank
-case object Two extends Rank
-case object Three extends Rank
-case object Four extends Rank
-case object Five extends Rank
-case object Six extends Rank
-case object Seven extends Rank
-case object Eight extends Rank
-case object Nine extends Rank
-case object Ten extends Rank
+
+case object `2` extends Rank
+
+case object `3` extends Rank
+
+case object `4` extends Rank
+
+case object `5` extends Rank
+
+case object `6` extends Rank
+
+case object `7` extends Rank
+
+case object `8` extends Rank
+
+case object `9` extends Rank
+
+case object `10` extends Rank
+
 case object Jack extends Rank
+
 case object Queen extends Rank
+
 case object King extends Rank
+
 case object Ace extends Rank
 
 sealed abstract class Card
-case class PlayingCard(rank: Rank, suite: Suite) extends Card {
+
+final case class PlayingCard(rank: Rank, suite: Suite) extends Card {
   override def toString: String = s"$rank of $suite"
 }
 
@@ -35,38 +54,47 @@ case object CutCard extends Card
 
 object Deck {
 
-  def new52(): Seq[Card] = {
-    val suites = Seq(♠, ♥, ♣, ♦)
-    val ranks = Seq(Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace)
+  def new52(): List[Card] = {
+    val suites = List(♠, ♥, ♣, ♦)
+    val ranks = List(`2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, Jack, Queen, King, Ace)
 
-      for {
-        s <- suites
-        r <- ranks
-      } yield PlayingCard(r, s)
+    for {
+      s <- suites
+      r <- ranks
+    } yield PlayingCard(r, s)
 
   }
 
-  def shuffle(deck: Seq[Card]): Seq[Card] = Random.shuffle(deck)
+  def shuffle(deck: List[Card]): List[Card] = Random.shuffle(deck)
+
+}
+
+object Shoe {
+
+  sealed trait Message
+
+  final case class Take(n: Int) extends Message
 
 }
 
 class Shoe extends Actor {
 
-   val decks: Seq[Card] = (for (i <- 1 to 6) yield Deck.shuffle(Deck.new52())).flatten
-   var cards: Seq[Card] = decks.take(260) ++ Seq(CutCard) ++ decks.drop(260)
+  val decks: List[Card] = (1 to 6).flatMap(_ => new52()).toList
+  var cards: List[Card] = Deck.shuffle(CutCard :: decks)
 
-   def receive() = {
-     case "Take" => {
+  def receive() = {
+    case Take(n) =>
 
-       println(cards.length)
-       println(cards.head)
+      println(cards.length)
+      println(cards.head)
 
-       cards = cards.drop(1)
+      cards = cards.drop(n)
 
-       println(cards.length)
+      println(cards.length)
 
-     }
-   }
+      println(cards.zipWithIndex.find(_._1 == CutCard).map(t => s"Cut Card is in position ${t._2}").getOrElse("No Cut Card"))
+
+  }
 
 }
 
@@ -81,8 +109,8 @@ object Main {
     println(cOne)
     println(cTwo)
 
-    val deckOne: Seq[Card] = Seq(cOne, cTwo, CutCard)
-    val deckTwo: Seq[Card] = Deck.shuffle(Deck.new52())
+    val deckOne: List[Card] = List(cOne, cTwo, CutCard)
+    val deckTwo: List[Card] = shuffle(new52())
 
     println(deckOne)
     println(deckTwo.length)
@@ -91,7 +119,7 @@ object Main {
     val system: ActorSystem = ActorSystem("helloAkka")
     val shoe = system.actorOf(Props[Shoe], name = "shoe")
 
-    shoe ! "Take"
+    shoe ! Take(3)
     system.terminate()
 
   }
